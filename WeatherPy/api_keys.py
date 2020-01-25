e9b9ad10293ba1c9c1278fe472d251229deebd25
alvinochieng <56371121+alvinochieng@users.noreply.github.com>
2020-01-25 10:01:10 -0500

    debugging first question

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2686abf95cbe027f5e396abb011061d3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBHJIdNIx4gUYQh0uStfCtDY3e8PEjPFZ8"
